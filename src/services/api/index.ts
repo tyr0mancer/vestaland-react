9ffaacd77376df876b6ae34085f17b82566cc2b6
Alex Groß <alex@superschurke.de>
2023-12-21 10:23:29 +0100

    fixed ssl

--- src/services/api/index.ts
+++ src/services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  //baseURL: 'https://api.vestaland.de/api'
-  baseURL: 'http://167.172.190.167:3000/api'
+  baseURL: 'https://api.vestaland.de/api'
+  //baseURL: 'http://167.172.190.167:3000/api'
 });
 
