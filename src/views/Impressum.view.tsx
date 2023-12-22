e2dc3fae400a98537c3e7cbdb029fac295925ca4
Alex Groß <alex@superschurke.de>
2023-12-22 11:50:56 +0100

    rel="noopener noreferrer"

--- src/views/Impressum.view.tsx
+++ src/views/Impressum.view.tsx
@@ -7,7 +7,7 @@ export function ImpressumView() {
     <MainMenu title="Vestaland"/>
     <Container className="main-layout">
       <h1>Anbieterkennung</h1>
-      <p>Icons: <a target="_blank" href="https://www.flaticon.com/authors/eucalyp">flaticon.com/authors/eucalyp</a></p>
+      <p>Icons: <a target="_blank" href="https://www.flaticon.com/authors/eucalyp" rel="noopener noreferrer">flaticon.com/authors/eucalyp</a></p>
       <p>Logo und weitere Bilder: Chad Peter</p>
       <p>Rezepte: geistes Eigentum vom Erfinder</p>
     </Container>
