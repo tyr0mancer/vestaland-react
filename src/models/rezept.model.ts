39f160b52633eef37995751eece7f53c79f26500
Alex Groß <alex@superschurke.de>
2023-12-26 00:15:12 +0100

    bugfix

--- src/models/rezept.model.ts
+++ src/models/rezept.model.ts
@@ -3,30 +3,35 @@ import {Hilfsmittel} from "./hilfsmittel.model";
 import {Benutzer} from "./benutzer.model";
 import {Datei} from "./datei.model";
 
-export class Aktion {
-  public name: string = "";
-  public beschreibung?: string;
+export class KochschrittMeta {
   public temperatur?: number;
   public hitze?: string;
-  public dauer?: number;
 }
 
-export class Arbeitsschritt {
-  public aktion?: Aktion;
-  public zutaten: Zutat[] = [];
-  public hilfsmittel: Hilfsmittel[]= [];
+export class Kochschritt {
+  public name: string = "";
+  public beschreibung?: string;
   public dauer?: number;
+  public zutaten: Zutat[] = [];
+  public hilfsmittel: Hilfsmittel[] = [];
+  public meta?: KochschrittMeta;
+}
+
+
+export class RezeptMeta {
+  public vegetarisch?: boolean;
+  public healthy?: boolean;
 }
 
 export class Rezept {
   public _id?: string;
   public name: string = '';
   public beschreibung?: string;
+  public portionen: number = 1;
   public author?: Benutzer;
   public bild?: Datei;
-
   public zutaten: Zutat[] = [];
   public hilfsmittel: Hilfsmittel[] = [];
-  public arbeitsschritte: Arbeitsschritt[] = [];
-  public portionen: number = 1;
+  public kochschritte: Kochschritt[] = [];
+  public meta?: RezeptMeta;
 }
