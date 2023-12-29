db67711b40dd75cc6c2adf86e5c4c69e67e30c5d
Alex Groß <alex@superschurke.de>
2023-12-29 12:19:13 +0100

    move up/down

--- src/components/rezept/RezeptForm.tsx
+++ src/components/rezept/RezeptForm.tsx
@@ -98,71 +98,84 @@ function KochSchritteForm({name, values: kochschritte}: FieldArrayFormProps<Koch
               <button type="button" onClick={() => arrayHelpers.remove(index)}>
                 - Kochschritt entfernen
               </button>
+
+              <button
+                type="button"
+                onClick={() => arrayHelpers.move(index, index - 1)}
+              >
+                Move Up
+              </button>
+              <button
+                type="button"
+                onClick={() => arrayHelpers.move(index, index + 1)}
+              >
+                Move Down
+              </button>
             </Box>
 
-          ))}
+            ))}
           <hr/>
           <button type="button" onClick={() => arrayHelpers.insert(kochschritte.length, new Kochschritt())}>
             neuer Kochschritt
           </button>
         </div>
       )}
-    />
-  );
-}
+      />
+      );
+      }
 
-function ZutatenForm({name, values: zutaten}: FieldArrayFormProps<Zutat>) {
-  return (
-    <FieldArray
+      function ZutatenForm({name, values: zutaten}: FieldArrayFormProps<Zutat>) {
+      return (
+      <FieldArray
       name={name}
       render={arrayHelpers => (
-        <div>
-          {zutaten.map((zutat: Zutat, index: number) => (
-            <div key={index}>
-              <Field name={`${name}[${index}][menge]`}/>
+      <div>
+    {zutaten.map((zutat: Zutat, index: number) => (
+      <div key={index}>
+      <Field name={`${name}[${index}][menge]`}/>
 
-              <Field name={`${name}[${index}][einheit]`}/>
-              <LebensmittelPicker
-                name={`${name}[${index}][lebensmittel]`}
-                values={zutat.lebensmittel || new Lebensmittel()}
-              />
-              <button type="button" onClick={() => arrayHelpers.remove(index)}>
-                - Zutat
-              </button>
-            </div>
-          ))}
-          <button type="button" onClick={() => arrayHelpers.insert(zutaten.length, new Zutat())}>
-            + Zutat
-          </button>
-        </div>
+      <Field name={`${name}[${index}][einheit]`}/>
+      <LebensmittelPicker
+      name={`${name}[${index}][lebensmittel]`}
+      values={zutat.lebensmittel || new Lebensmittel()}
+      />
+      <button type="button" onClick={() => arrayHelpers.remove(index)}>
+      - Zutat
+      </button>
+      </div>
+      ))}
+      <button type="button" onClick={() => arrayHelpers.insert(zutaten.length, new Zutat())}>
+      + Zutat
+      </button>
+      </div>
       )}
-    />
-  )
-}
+      />
+      )
+    }
 
 
-function HilfsmittelForm({name, values}: FieldArrayFormProps<Hilfsmittel>) {
-  return (
-    <FieldArray
-      name={name}
-      render={arrayHelpers => (
+      function HilfsmittelForm({name, values}: FieldArrayFormProps<Hilfsmittel>) {
+        return (
+        <FieldArray
+        name={name}
+        render={arrayHelpers => (
         <div>
-          {values.map((hilfsmittel: Hilfsmittel, index: number) => (
-            <div key={index}>
-              <HilfsmittelPicker
-                name={`${name}[${index}]`}
-                values={hilfsmittel}
-              />
-              <button type="button" onClick={() => arrayHelpers.remove(index)}>
-                - HM
-              </button>
-            </div>
-          ))}
-          <button type="button" onClick={() => arrayHelpers.insert(values.length, new Hilfsmittel())}>
-            + HM
-          </button>
+      {values.map((hilfsmittel: Hilfsmittel, index: number) => (
+        <div key={index}>
+        <HilfsmittelPicker
+        name={`${name}[${index}]`}
+        values={hilfsmittel}
+        />
+        <button type="button" onClick={() => arrayHelpers.remove(index)}>
+        - HM
+        </button>
         </div>
-      )}
-    />
-  );
-}
+        ))}
+        <button type="button" onClick={() => arrayHelpers.insert(values.length, new Hilfsmittel())}>
+        + HM
+        </button>
+        </div>
+        )}
+        />
+        );
+      }
