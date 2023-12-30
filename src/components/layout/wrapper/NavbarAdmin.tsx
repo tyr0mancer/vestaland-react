7f8d12f83a2aa0f04b312bfc6b6f4d0bd13d8a3a
Alex Groß <alex@superschurke.de>
2023-12-30 15:15:39 +0100

    removed navbar admin

--- src/components/layout/wrapper/NavbarAdmin.tsx
+++ src/components/layout/wrapper/NavbarAdmin.tsx
@@ -1,35 +1,8 @@
 import React from 'react';
-
-import {Navbar, Nav, Container, Button} from 'react-bootstrap';
-import {NavLink, useNavigate} from 'react-router-dom';
-import {FaCirclePlus} from "react-icons/fa6";
-import {FaBackward} from "react-icons/fa6";
-
+import {Link} from 'react-router-dom';
 export function NavbarAdmin() {
-  const navigate = useNavigate();
-
-  return (<>
-    <Navbar expand="lg" sticky="top" className="main-nav admin">
-      <Container>
-        <Navbar.Text>
-          <Button variant="primary" size="sm">
-            <FaBackward onClick={()=>{navigate('/rezepte')}}/>
-          </Button>
-        </Navbar.Text>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={NavLink} to={'/admin/lebensmittel'}>Lebensmittel</Nav.Link>
-            <Nav.Link as={NavLink} to={'/admin/hilfsmittel'}>Hilfsmittel</Nav.Link>
-            <Nav.Link as={NavLink} to={'/admin/user'}>Benutzer</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Text>
-          <Button variant="primary" size="sm">
-            <FaCirclePlus />
-          </Button>
-        </Navbar.Text>
-      </Container>
-    </Navbar>
+    return (<>
+    <h1>NavbarAdmin</h1>
+    <Link to={'/'}>back</Link>
   </>)
 }
