1fbf1a094853102a7135fa1007434db5ac4e068c
Alex Groß <alex@superschurke.de>
2024-01-11 10:23:22 +0100

    StartCookingButton

--- src/components/common/ui/StartCookingButton.tsx
+++ src/components/common/ui/StartCookingButton.tsx
@@ -6,6 +6,7 @@ import {ActionTypes, StateContextType} from "../../../util/state/types";
 import {Blender} from "@mui/icons-material";
 import {IconButton} from "@mui/material";
 import {customConfirm} from "./ConfirmDialog";
+import {Kochschritt} from "../../../shared-types/models/Kochschritt";
 
 interface StartCookingButtonProps {
   rezept: Rezept
@@ -24,9 +25,29 @@ export function StartCookingButton({rezept}: StartCookingButtonProps): React.Rea
     const result = !rezeptCooking || await customConfirm({title: 'Es wird bereits gekocht.', label: rezeptCooking.name})
     if (!result) return
 
+    const summary = rezept.kochschritte.reduce((previousValue: any[], currentValue: Kochschritt) => {
+
+      let gesamtdauer = currentValue.gesamtdauer || ((currentValue.arbeitszeit || 0) + (currentValue.wartezeit || 0))
+      let arbeitszeit = currentValue.arbeitszeit || 0
+
+      let ratio = null
+      if (arbeitszeit && gesamtdauer) ratio = arbeitszeit / gesamtdauer
+
+      const newValue = {
+        length: gesamtdauer,
+        ratio: ratio,
+        aktionen: currentValue.aktionen
+      }
+
+      return [...previousValue, newValue]
+    }, [])
+
     dispatch({type: ActionTypes.PUSH_HISTORY, payload: rezept})
     dispatch({type: ActionTypes.SET_REZEPT_COOK, payload: rezept})
-    dispatch({type: ActionTypes.SET_KOCHSTATUS, payload: {kochschrittIndex: -1, kochschrittFokus: false}})
+    dispatch({
+      type: ActionTypes.SET_KOCHSTATUS,
+      payload: {kochschrittIndex: -1, kochschrittFokus: false, kochschrittSummary: summary}
+    })
     navigate('/rezept-cooking')
   }
 
