7d00d756cdabf886c6d7eb0166873722c55eb641
Alex Groß <alex@superschurke.de>
2024-01-17 21:59:56 +0100

    LebensmittelForm.tsx

--- src/components/pages/admin/forms/LebensmittelForm.tsx
+++ src/components/pages/admin/forms/LebensmittelForm.tsx
@@ -4,13 +4,12 @@ import {CustomTextField} from "../../../common/form-elements/generic/CustomTextF
 import {CustomCheckbox} from "../../../common/form-elements/generic/CustomCheckbox";
 import {CustomSelect} from "../../../common/form-elements/generic/CustomSelect";
 
-type LebensmittelFormProps = {}
 
 /**
  * Layout für ein Formular zur Eingabe und Verwaltung von Lebensmitteln
  * Sollte innerhalb eines <Formik></Formik> Blocks sein
  */
-export function LebensmittelForm({}: LebensmittelFormProps): React.ReactElement {
+export function LebensmittelForm(): React.ReactElement {
   return (<>
     <CustomTextField name={'name'} label={'Lebensmittel-Name'}/>
     <CustomCheckbox name={'publicVisible'} label={'öffentlich'} />
